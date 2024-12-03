db0d9314c9beb50fd9e8f450b39598cc6d2b2082
Lenson200 <136510914+Lenson200@users.noreply.github.com>
2024-12-03 13:43:38 +0000

    adding final touches

--- Training/context_processors.py
+++ Training/context_processors.py
@@ -1,11 +1,9 @@
 from django.shortcuts import get_object_or_404
 from Training.models import EmployeeProfile
 
+
 def profile_context(request):
     if request.user.is_authenticated:
-        try:
-            profile = get_object_or_404(EmployeeProfile, user=request.user)
-            return {'profile': profile}
-        except EmployeeProfile.DoesNotExist:
-            return {'profile': None}
+        profile = EmployeeProfile.objects.filter(user=request.user).first()
+        return {'profile': profile}
     return {'profile': None}
--- Training/urls.py
+++ Training/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from django.conf.urls.static import static
 from Training import views
+
 urlpatterns = [
-    path("",views.index,name="index"),
+    path('',views.index,name="index"),
     path('search/', views.search, name='search'),
     path("login",views.login_view, name="login"),
     path("Register",views.register,name="register"),
@@ -10,7 +11,6 @@ urlpatterns = [
     path("profileview", views.profile_view,name="profileview"),
     path('addmodules/',views.training_module_create,name='addmodules'),
     path('add-completed-training/', views.add_completed_trainings, name='add_completed_training'),
-    path('employee/<int:employee_id>/', views.employee_trainings, name='employee_trainings'),
     path('training-modules/', views.training_module_list, name='training_module_list'),
    path('training_module/<int:pk>/delete/', views.training_module_delete, name='training_module_delete'),
     path('training-modules/<int:pk>/', views.training_module_detail, name='training_module_detail'),
