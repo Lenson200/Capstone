9a6d76462d37ce3467d8b5e839f4399a9a95f61b
Lenson200 <136510914+Lenson200@users.noreply.github.com>
2024-12-16 12:16:49 +0300

    Fix STATIC_ROOT configuration

--- LearningManagementSystem/settings.py
+++ LearningManagementSystem/settings.py
@@ -121,7 +121,7 @@ MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = "/static/"
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
